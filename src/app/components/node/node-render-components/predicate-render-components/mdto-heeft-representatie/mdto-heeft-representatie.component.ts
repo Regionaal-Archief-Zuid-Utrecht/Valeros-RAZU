86f68b0cf772070ed1bb9a2ca3fab0a981ee03a3
Simon <Simon-Dirks@users.noreply.github.com>
2024-12-24 13:15:41 +0100

    Show original link for heeftRepresentatie

--- src/app/components/node/node-render-components/predicate-render-components/mdto-heeft-representatie/mdto-heeft-representatie.component.ts
+++ src/app/components/node/node-render-components/predicate-render-components/mdto-heeft-representatie/mdto-heeft-representatie.component.ts
@@ -2,11 +2,12 @@ import { Component, type OnInit } from '@angular/core';
 import { HopComponent } from '../hop-components/hop.component';
 import { JsonPipe, NgForOf, NgIf } from '@angular/common';
 import { MdtoUrlBestandComponent } from '../mdto-url-bestand/mdto-url-bestand.component';
+import { NodeLinkComponent } from "../../../node-link/node-link.component";
 
 @Component({
   selector: 'app-mdto-heeft-representatie',
   standalone: true,
-  imports: [NgIf, NgForOf, JsonPipe, MdtoUrlBestandComponent],
+  imports: [NgIf, NgForOf, JsonPipe, MdtoUrlBestandComponent, NodeLinkComponent],
   templateUrl: './mdto-heeft-representatie.component.html',
   styleUrl: './mdto-heeft-representatie.component.css',
 })
