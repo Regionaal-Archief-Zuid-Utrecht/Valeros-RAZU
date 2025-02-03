7b357b451d936386636bf25fb23cfae8ed805099
Wietse Bakker <w.bakker@razu.nl>
2025-02-03 09:53:02 +0100

    correction sparql

--- src/app/config/settings.ts
+++ src/app/config/settings.ts
@@ -140,12 +140,12 @@ export const Settings = {
         {
           elastic:
             'https://api.data.razu.nl/datasets/id/object/services/object/_search',
-          sparql: 'https://api.data.razu.nl/datasets/id/object/sparql2',
+          sparql: 'https://api.data.razu.nl/datasets/id/object/sparql',
         },
         // This is how an enriched endpoint would look like
         {
           elastic: 'https://ontwikkel.viewer.razu.nl/sura/_search',
-          sparql: 'https://api.data.razu.nl/datasets/id/object/sparql2',
+          sparql: 'https://api.data.razu.nl/datasets/id/object/sparql',
         },
       ],
     },
