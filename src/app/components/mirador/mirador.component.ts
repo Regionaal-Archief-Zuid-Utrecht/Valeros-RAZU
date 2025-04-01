8d0e48882ce8916a409602d5fd9ddbdacf7fc791
Simon Dirks <mail@simondirks.com>
2025-04-01 11:30:55 +0200

    Refactor Mirador into its own component

---
+++ src/app/components/mirador/mirador.component.ts
@@ -0,0 +1,90 @@
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  NgZone,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges,
+  ViewEncapsulation,
+} from '@angular/core';
+import { NgClass } from '@angular/common';
+// @ts-ignore
+import Mirador from 'mirador/dist/es/src/index';
+import { IIIFService } from '../../services/iiif.service';
+
+@Component({
+  selector: 'app-mirador',
+  standalone: true,
+  templateUrl: './mirador.component.html',
+  styleUrl: './mirador.component.scss',
+})
+export class MiradorComponent implements OnChanges, OnDestroy, AfterViewInit {
+  private _viewer?: any;
+
+  @Input() imageUrls?: string[];
+
+  constructor(
+    private ngZone: NgZone,
+    private iiifService: IIIFService,
+  ) {}
+
+  ngAfterViewInit() {
+    this.initViewer(this.imageUrls);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['imageUrls']) {
+      this.initViewer(this.imageUrls);
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroyViewer();
+  }
+
+  private destroyViewer() {
+    if (this._viewer) {
+      this._viewer = undefined;
+    }
+  }
+
+  private initViewer(imgUrls: string[] | undefined) {
+    if (!imgUrls || imgUrls.length === 0) {
+      return;
+    }
+
+    this.destroyViewer();
+
+    this._viewer = this.ngZone.runOutsideAngular(() => {
+      const manifestUrl = this.iiifService.createManifestBlob(imgUrls);
+      const containerId = 'mirador';
+      const containerElem = document.getElementById(containerId);
+      if (!containerElem) {
+        console.warn('Container element not found', containerId);
+        return;
+      }
+
+      return Mirador.viewer({
+        id: 'mirador',
+        workspace: {
+          type: 'single',
+          showZoomControls: true,
+        },
+        workspaceControlPanel: {
+          enabled: false,
+        },
+        windows: [
+          {
+            manifestId: manifestUrl,
+            allowWindowSideBar: true,
+            sideBarOpenByDefault: false,
+            allowMaximize: false,
+            allowFullscreen: true,
+            allowClose: false,
+          },
+        ],
+      });
+    });
+  }
+}
--- src/app/components/node/node-images/node-images.component.ts
+++ src/app/components/node/node-images/node-images.component.ts
@@ -1,142 +1,22 @@
-import { IIIFService } from '../../../services/iiif.service';
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  Input,
-  NgZone,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-  SimpleChanges,
-  ViewChild,
-  ViewEncapsulation,
-} from '@angular/core';
-import { JsonPipe, NgClass, NgForOf, NgIf } from '@angular/common';
-import { Config } from '../../../config/config';
-import { UrlService } from '../../../services/url.service';
-import { Settings } from '../../../config/settings';
-// @ts-ignore
-import Mirador from 'mirador/dist/es/src/index';
-import { NodeLinkComponent } from '../node-link/node-link.component';
+import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { MiradorComponent } from '../../../components/mirador/mirador.component';
 
 @Component({
   selector: 'app-node-images',
   standalone: true,
-  imports: [NgForOf, NgIf, JsonPipe, NgClass, NodeLinkComponent],
+  imports: [NgIf, MiradorComponent],
   templateUrl: './node-images.component.html',
   styleUrl: './node-images.component.scss',
-  encapsulation: ViewEncapsulation.None,
 })
-export class NodeImagesComponent
-  implements OnInit, OnChanges, OnDestroy, AfterViewInit
-{
-  private _imageViewer?: any;
-
+export class NodeImagesComponent {
   @Input() imageUrls?: string[];
   @Input() shownInTableCell = true;
   @Input() useViewer = true;
   @Input() imageLabel?: string;
 
-  constructor(
-    public urlService: UrlService,
-    private ngZone: NgZone,
-    private iiifService: IIIFService,
-  ) {}
-
-  ngOnInit() {}
-
-  ngAfterViewInit() {
-    this.initImageViewer(this.imageUrls);
+  onImageLoadError(event: Event) {
+    const imgElement = event.target as HTMLImageElement;
+    imgElement.src = '/assets/images/default.png';
   }
-
-  destroyImageViewer() {
-    if (this._imageViewer) {
-      this._imageViewer = undefined;
-    }
-  }
-
-  initImageViewer(imgUrls: string[] | undefined) {
-    if (!this.useViewer) {
-      return;
-    }
-    if (!imgUrls || imgUrls.length === 0) {
-      return;
-    }
-
-    this.destroyImageViewer();
-
-    this._imageViewer = this.ngZone.runOutsideAngular(() => {
-      const manifestUrl = this.iiifService.createManifestBlob(imgUrls);
-      const containerId = 'mirador';
-      const containerElem = document.getElementById(containerId);
-      if (!containerElem) {
-        console.warn('Container element not found', containerId);
-        return;
-      }
-      // console.log(
-      //   'Manifest URL',
-      //   manifestUrl,
-      // );
-
-      const miradorInstance = Mirador.viewer({
-        id: 'mirador',
-        workspace: {
-          type: 'single',
-          showZoomControls: true,
-        },
-        workspaceControlPanel: {
-          enabled: false,
-        },
-        windows: [
-          {
-            manifestId: manifestUrl,
-            allowWindowSideBar: true,
-            sideBarOpenByDefault: false,
-            allowMaximize: false,
-            allowFullscreen: true,
-            allowClose: false,
-          },
-        ],
-      });
-
-      // Center image after load
-      miradorInstance.store.subscribe(() => {
-        const state = miradorInstance.store.getState();
-        const windows = state.windows || {};
-        const windowIds = Object.keys(windows);
-
-        if (windowIds.length > 0) {
-          const windowId = windowIds[0];
-          const canvasId = windows[windowId]?.canvasId;
-
-          if (canvasId && state.viewers?.[windowId]?.viewer) {
-            const viewer = state.viewers[windowId].viewer;
-            if (viewer && !viewer.__centered) {
-              viewer.viewport.goHome();
-              viewer.__centered = true;
-            }
-          }
-        }
-      });
-
-      return miradorInstance;
-    });
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['imageUrls']) {
-      this.initImageViewer(this.imageUrls);
-    }
-  }
-
-  ngOnDestroy() {
-    this.destroyImageViewer();
-  }
-
-  onImageLoadError($event: ErrorEvent) {
-    ($event.target as any).src = Settings.imageForWhenLoadingFails;
-  }
-
-  protected readonly Config = Config;
 }
