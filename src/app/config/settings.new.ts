dbae4138148a30a66f4ba8e7cda0343cc0ff4971
Simon Dirks <mail@simondirks.com>
2025-04-24 10:48:10 +0200

    Remove unused settings file

--- src/app/config/settings.new.ts
+++ src/app/config/settings.new.ts
@@ -2,7 +2,6 @@ import { clusteringSettings } from './settings/clustering.settings';
 import { endpointSettings } from './settings/endpoint.settings';
 import { fileTypeSettings } from './settings/file-types.settings';
 import { filteringSettings } from './settings/filtering.settings';
-import { headerSettings } from './settings/header.settings';
 import { matomoSettings } from './settings/matomo.settings';
 import { miscSettings } from './settings/misc.settings';
 import { predicateRenderSettings } from './settings/render.settings';
@@ -13,7 +12,6 @@ export const Settings = {
   endpoints: endpointSettings,
   filtering: filteringSettings,
   fileTypes: fileTypeSettings,
-  header: headerSettings,
   sorting: sortingSettings,
   search: searchSettings,
   clustering: clusteringSettings,
