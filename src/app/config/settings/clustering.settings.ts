c1e4508f9469f274b2b4faf154e769b85f2e326d
Simon Dirks <mail@simondirks.com>
2025-04-24 10:40:24 +0200

    Add clustering example

--- src/app/config/settings/clustering.settings.ts
+++ src/app/config/settings/clustering.settings.ts
@@ -4,3 +4,28 @@ export const clusteringSettings: ClusteringSettings = {
   filterOptionValues: {},
   types: {},
 };
+
+// Example:
+// filterOptionValues: {
+//   locations: {
+//     label: 'Locaties',
+//     valueIds: [
+//       'https://schema.org/Place',
+//       'https://schema.org/PostalAddress',
+//       'https://www.ica.org/standards/RiC/ontology#Place',
+//       'https://data.razu.nl/def/ldto/dekkingInRuimte',
+//       'http://www.opengis.net/ont/geosparql#Geometry',
+//     ],
+//   },
+// }
+
+// Example:
+// types: {
+//   recordSet: {
+//     label: 'RecordSet',
+//     valueIds: [
+//       'https://www.ica.org/standards/RiC/ontology#RecordSet',
+//       'https://www.ica.org/standards/RiC/vocabularies/recordSetTypes#File',
+//     ],
+//   },
+// },
