872d4fd952626a93ab3c12b7efed181850632858
Simon Dirks <mail@simondirks.com>
2025-05-27 14:51:37 +0200

    Fix RAZU sorting issue

--- src/app/config/razu-settings/razu-sorting.settings.ts
+++ src/app/config/razu-settings/razu-sorting.settings.ts
@@ -1,6 +1,21 @@
+import { SortOrder } from '../../models/settings/sort-order.enum';
 import { SortingSettings } from '../../models/settings/sorting-settings.model';
 import { sortingSettings } from '../default-settings/sorting.settings';
+import { razuLabelPredicates } from './razu-predicate.settings';
 
 export const razuSortingSettings: SortingSettings = {
   ...sortingSettings,
+  options: {
+    ...sortingSettings.options,
+    'title-a-z': {
+      fields: razuLabelPredicates,
+      label: 'Titel (A-Z)',
+      order: SortOrder.Ascending,
+    },
+    'title-z-a': {
+      fields: razuLabelPredicates,
+      label: 'Titel (Z-A)',
+      order: SortOrder.Descending,
+    },
+  },
 };
