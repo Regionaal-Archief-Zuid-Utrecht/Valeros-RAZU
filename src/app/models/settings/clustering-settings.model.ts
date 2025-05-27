c047a2f962322ad2ed634e6f1309d0951e51e716
Simon Dirks <mail@simondirks.com>
2025-05-27 15:08:03 +0200

    Add JSDoc comments

--- src/app/models/settings/clustering-settings.model.ts
+++ src/app/models/settings/clustering-settings.model.ts
@@ -1,6 +1,14 @@
 import { ClusterValuesSettings } from './cluster-values-settings.model';
 
 export interface ClusteringSettings {
+  /**
+   * If you notice that multiple filter option values refer to the same (or a similar) thing, you might want to show them as a single value, to streamline the user experience.
+   * As an example, if you use multiple ontologies and create a filter for node types, there might be multiple values (e.g. sdo:Place, rico:Place, etc.) that refer to the "Location" of a node, which could be clustered into a single "Location" value.
+   */
   filterOptionValues: ClusterValuesSettings;
+  /**
+   * If you notice that multiple type IRIs refer to the same (or a similar) thing, you might want to show them as a (clustered) single type, to streamline the user experience.
+   */
+  // TODO: Add example
   types: ClusterValuesSettings;
 }
--- src/app/models/settings/predicate-settings.model.ts
+++ src/app/models/settings/predicate-settings.model.ts
@@ -2,6 +2,15 @@ export interface PredicateSettings {
   parents: string[];
   label: string[];
   type: string[];
+  /**
+   * This setting determines which predicates are used to consider node values to be files.
+   * This is used to show a file (e.g. image, PDF, ...) preview next to the node table (if it's a renderable file type).
+   */
   files: string[];
+  /**
+   * Sometimes, node files are not directly linked to the node, but a number of hops away.
+   * This setting makes the hops (in sequential order) to find the files.
+   * This is used to show a file (e.g. image, PDF, ...) preview next to the node table (if it's a renderable file type).
+   */
   hopFiles: string[][];
 }
