972660eb290ba78796bbc594211868b1ceab5541
Simon Dirks <mail@simondirks.com>
2025-05-27 12:06:07 +0200

    Update JSDoc comment

--- src/app/config/default-settings/search.settings.ts
+++ src/app/config/default-settings/search.settings.ts
@@ -3,6 +3,6 @@ import { autocompleteSettings } from './autocomplete.settings';
 
 export const searchSettings: SearchSettings = {
   resultsPerPagePerEndpoint: 10,
-  elasticTopHitsMax: 100, // Mainly used for counting filter option hits (e.g. if there are more than 100 "public domain" values found for the "license" filter, show "100+"). Max 100 by default for elastic, tweak index.max_inner_result_window to go higher
+  elasticTopHitsMax: 100,
   autocomplete: autocompleteSettings,
 };
