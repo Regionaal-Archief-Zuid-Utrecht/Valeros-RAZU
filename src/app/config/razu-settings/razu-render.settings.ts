3b409aa4ff6c357e6f4b8b327e78d6da5d68aa81
Simon Dirks <mail@simondirks.com>
2025-05-27 12:04:12 +0200

    Remove unused settings files

--- src/app/config/razu-settings/razu-render.settings.ts
+++
@@ -1,6 +0,0 @@
-import { PredicateRenderSettings } from '../default-settings/render.settings';
-import { predicateRenderSettings } from '../default-settings/render.settings';
-
-export const razuPredicateRenderSettings: PredicateRenderSettings = {
-  ...predicateRenderSettings,
-};
