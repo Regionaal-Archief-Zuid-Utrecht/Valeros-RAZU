2f127251c25a1d07854a34ee9c8444b754e7a142
Simon Dirks <mail@simondirks.com>
2025-05-27 11:28:13 +0200

    Separate and update namespace prefixes settings

--- src/app/config/razu-settings/razu-namespace-prefixes.settings.ts
+++ src/app/config/razu-settings/razu-namespace-prefixes.settings.ts
@@ -2,4 +2,7 @@ import { namespacePrefixes } from '../default-settings/namespace-prefixes.settin
 
 export const razuNamespacePrefixes: Record<string, string> = {
   ...namespacePrefixes,
+  'http://www.nationaalarchief.nl/mdto-shacl#': 'mdto-sh:',
+  'https://data.razu.nl/def/ldto/': 'ldto:',
+  'https://data.razu.nl/razu/': 'razu:',
 };
