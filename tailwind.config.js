73b7e0f46a40bba01a14c2a1c296748cc8b5c26d
Simon Dirks <mail@simondirks.com>
2025-06-02 11:10:16 +0200

    Use production colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,17 +22,17 @@ module.exports = {
         main: {
           ...require("daisyui/src/theming/themes")["light"],
           // PRODUCTION:
-          // primary: "#6c5a82",
-          // neutral: "#a999bd",
-          // accent: "#9ad199", // Used for highlighting search results
-          // secondary: "#c2b280",
-          // "primary-content": "white",
-          // background: "#E5E4E2",
-          // search_background: "#E5E4E2",
-          // "--rounded-box": "0.375rem",
-          // "--rounded-btn": "0.375rem",
-          // "--rounded-badge": "0.375rem",
-          // black: "#000000",
+          primary: "#6c5a82",
+          neutral: "#a999bd",
+          accent: "#9ad199", // Used for highlighting search results
+          secondary: "#c2b280",
+          "primary-content": "white",
+          background: "#E5E4E2",
+          search_background: "#E5E4E2",
+          "--rounded-box": "0.375rem",
+          "--rounded-btn": "0.375rem",
+          "--rounded-badge": "0.375rem",
+          black: "#000000",
 
           // // DEVELOPMENT:
           // primary: "#4a4a4a", // Dark gray for primary elements
@@ -47,17 +47,17 @@ module.exports = {
           // "--rounded-badge": "1rem",
           // // black: "#000000",
 
-          // ACCEPT:
-          primary: "#2e7d32", // Dark green for primary elements
-          neutral: "#66bb6a", // Medium green for neutral elements
-          accent: "#a5d6a7", // Light green for highlighting search results
-          secondary: "#388e3c", // A slightly darker green for secondary elements
-          "primary-content": "white", // White text to maintain contrast
-          background: "#e8f5e9", // Very light green for the background
-          search_background: "#e8f5e9", // Match background color
-          "--rounded-box": "1rem",
-          "--rounded-btn": "1rem",
-          "--rounded-badge": "1rem",
+          // // ACCEPT:
+          // primary: "#2e7d32", // Dark green for primary elements
+          // neutral: "#66bb6a", // Medium green for neutral elements
+          // accent: "#a5d6a7", // Light green for highlighting search results
+          // secondary: "#388e3c", // A slightly darker green for secondary elements
+          // "primary-content": "white", // White text to maintain contrast
+          // background: "#e8f5e9", // Very light green for the background
+          // search_background: "#e8f5e9", // Match background color
+          // "--rounded-box": "1rem",
+          // "--rounded-btn": "1rem",
+          // "--rounded-badge": "1rem",
 
           // // TEST:
           // primary: "#1565c0", // Dark blue for primary elements
