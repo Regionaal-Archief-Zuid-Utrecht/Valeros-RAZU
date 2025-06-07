88e3917a9ec5dd329dbc4ef092730ae07ffdd5b1
Wietse Bakker <w.bakker@razu.nl>
2025-06-07 16:51:00 +0200

    Getting somewhere with the hop-links to the krant

--- src/app/components/custom-render-components/by-type/razu-aflevering/razu-aflevering.component.ts
+++ src/app/components/custom-render-components/by-type/razu-aflevering/razu-aflevering.component.ts
@@ -1,26 +1,10 @@
 import { JsonPipe, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { TypeRenderComponent } from '../type-render-component.component';
-import { SparqlService } from '../../../../services/sparql.service';
-import { NodeService } from '../../../../services/node/node.service';
 import { HopLinkComponent } from '../../../features/node/node-render-components/predicate-render-components/hop-components/hop-link/hop-link.component';
-import { NodeModel } from '../../../../models/node.model';
-import { HopLinkSettings } from '../../../../models/settings/hop-link-settings.model';
 import { LdtoDekkingInTijdComponent } from '../../../custom-render-components/by-predicate/ldto-dekking-in-tijd/ldto-dekking-in-tijd.component';
-
-interface RelatedData {
-    id: string;
-    naam?: string;
-    dekkingInRuimte?: string;
-    omschrijving?: string;
-}
-
-interface BeperkingGebruik {
-    id: string;
-    prefLabel?: string;
-    copyrightNotice?: string;
-    note?: string;
-}
+import { HopLinkSettings } from '../../../../models/settings/hop-link-settings.model';
+import { TypeRenderComponentInput } from '../../../../models/type-render-component-input.model';
 
 @Component({
     selector: 'app-razu-aflevering',
@@ -29,126 +13,78 @@ interface BeperkingGebruik {
     templateUrl: './razu-aflevering.component.html',
     styleUrls: ['./razu-aflevering.component.scss'],
 })
-export class RazuAfleveringComponent
-    extends TypeRenderComponent
-    implements OnInit {
-
-    // Related data
+export class RazuAfleveringComponent extends TypeRenderComponent implements OnInit {
+    // Arrays to store IDs retrieved from hop-link components
     onderdeelVanIds: string[] = [];
-    relatedData: RelatedData[] = [];
-
-    // Beperking gebruik data
     beperkingGebruikIds: string[] = [];
-    beperkingGebruikData: BeperkingGebruik[] = [];
+    dekkingInTijdIds: string[] = [];
+    dekkingInRuimteIds: string[] = [];
+    hasBeginDate = false;
+    hasEndDate = false;
+    hasType = false;
 
+    // Loading state
     loading = false;
 
-    constructor(
-        private sparql: SparqlService,
-        private nodeService: NodeService
-    ) {
+    // Explicitly declare data property from parent class for template access
+    override data?: TypeRenderComponentInput;
+
+    // Hop settings for isOnderdeelVan
+    onderdeelVanSettings: HopLinkSettings = {
+        preds: ['https://data.razu.nl/def/ldto/isOnderdeelVan'],
+        showHops: false,
+        showOriginalLink: false
+    };
+
+    // Hop settings for beperkingGebruik
+    beperkingGebruikSettings: HopLinkSettings = {
+        preds: ['https://data.razu.nl/def/ldto/beperkingGebruik'],
+        showHops: false,
+        showOriginalLink: false
+    };
+
+    // Hop settings for related properties
+    naamSettings: HopLinkSettings = {
+        preds: ['https://data.razu.nl/def/ldto/naam'],
+        showHops: false,
+        showOriginalLink: false
+    };
+
+    dekkingInRuimteSettings: HopLinkSettings = {
+        preds: ['https://data.razu.nl/def/ldto/dekkingInRuimte'],
+        showHops: false,
+        showOriginalLink: false
+    };
+
+    omschrijvingSettings: HopLinkSettings = {
+        preds: ['https://data.razu.nl/def/ldto/omschrijving'],
+        showHops: false,
+        showOriginalLink: false
+    };
+
+    prefLabelSettings: HopLinkSettings = {
+        preds: ['http://www.w3.org/2004/02/skos/core#prefLabel'],
+        showHops: false,
+        showOriginalLink: false
+    };
+
+    copyrightNoticeSettings: HopLinkSettings = {
+        preds: ['http://schema.org/copyrightNotice'],
+        showHops: false,
+        showOriginalLink: false
+    };
+
+    noteSettings: HopLinkSettings = {
+        preds: ['http://www.w3.org/2004/02/skos/core#note'],
+        showHops: false,
+        showOriginalLink: false
+    };
+
+    constructor() {
         super();
     }
 
     ngOnInit(): void {
-        if (this.data?.node) {
-            this.initRelatedData();
-        }
-    }
-
-    async initRelatedData(): Promise<void> {
-        this.loading = true;
-
-        // Get isOnderdeelVan IDs
-        this.onderdeelVanIds = this.nodeService.getObjValues(
-            this.data?.node,
-            ['https://data.razu.nl/def/ldto/isOnderdeelVan']
-        );
-
-        // Get beperkingGebruik IDs
-        this.beperkingGebruikIds = this.nodeService.getObjValues(
-            this.data?.node,
-            ['https://data.razu.nl/def/ldto/beperkingGebruik']
-        );
-
-        // Fetch related data for isOnderdeelVan
-        if (this.onderdeelVanIds.length > 0) {
-            await this.fetchOnderdeelVanData();
-        }
-
-        // Fetch related data for beperkingGebruik
-        if (this.beperkingGebruikIds.length > 0) {
-            await this.fetchBeperkingGebruikData();
-        }
-
-        this.loading = false;
-    }
-
-    async fetchOnderdeelVanData(): Promise<void> {
-        const promises = this.onderdeelVanIds.map(async (id) => {
-            const relatedItem: RelatedData = { id };
-
-            // Get naam
-            const naamIds = await this.sparql.getObjIds(id, ['https://data.razu.nl/def/ldto/naam']);
-            if (naamIds.length > 0) {
-                const nodeData = await this.sparql.getNode(naamIds[0]);
-                const literals = this.nodeService.getObjValues(nodeData, ['http://www.w3.org/1999/02/22-rdf-syntax-ns#value']);
-                if (literals.length > 0) {
-                    relatedItem.naam = literals[0];
-                }
-            }
-
-            // Get dekkingInRuimte
-            const dekkingInRuimteIds = await this.sparql.getObjIds(id, ['https://data.razu.nl/def/ldto/dekkingInRuimte']);
-            if (dekkingInRuimteIds.length > 0) {
-                const nodeData = await this.sparql.getNode(dekkingInRuimteIds[0]);
-                const literals = this.nodeService.getObjValues(nodeData, ['http://www.w3.org/1999/02/22-rdf-syntax-ns#value']);
-                if (literals.length > 0) {
-                    relatedItem.dekkingInRuimte = literals[0];
-                }
-            }
-
-            // Get omschrijving
-            const omschrijvingIds = await this.sparql.getObjIds(id, ['https://data.razu.nl/def/ldto/omschrijving']);
-            if (omschrijvingIds.length > 0) {
-                const nodeData = await this.sparql.getNode(omschrijvingIds[0]);
-                const literals = this.nodeService.getObjValues(nodeData, ['http://www.w3.org/1999/02/22-rdf-syntax-ns#value']);
-                if (literals.length > 0) {
-                    relatedItem.omschrijving = literals[0];
-                }
-            }
-
-            return relatedItem;
-        });
-
-        this.relatedData = await Promise.all(promises);
-    }
-
-    async fetchBeperkingGebruikData(): Promise<void> {
-        const promises = this.beperkingGebruikIds.map(async (id) => {
-            const beperkingItem: BeperkingGebruik = { id };
-
-            // Get prefLabel
-            const prefLabelIds = await this.sparql.getObjIds(id, ['http://www.w3.org/2004/02/skos/core#prefLabel']);
-            if (prefLabelIds.length > 0) {
-                beperkingItem.prefLabel = prefLabelIds[0];
-            }
-
-            // Get copyrightNotice
-            const copyrightIds = await this.sparql.getObjIds(id, ['http://schema.org/copyrightNotice']);
-            if (copyrightIds.length > 0) {
-                beperkingItem.copyrightNotice = copyrightIds[0];
-            }
-
-            // Get note
-            const noteIds = await this.sparql.getObjIds(id, ['http://www.w3.org/2004/02/skos/core#note']);
-            if (noteIds.length > 0) {
-                beperkingItem.note = noteIds[0];
-            }
-
-            return beperkingItem;
-        });
-
-        this.beperkingGebruikData = await Promise.all(promises);
+        // No initialization needed as the hop-link components will handle data fetching
     }
 }
