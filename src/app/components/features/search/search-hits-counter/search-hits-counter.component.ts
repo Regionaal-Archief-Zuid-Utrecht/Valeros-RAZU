26e40c6cf453b042f7ce1360fd1a42d4ef2b1765
Simon Dirks <mail@simondirks.com>
2025-07-15 15:57:14 +0200

    Remove unused import

--- src/app/components/features/search/search-hits-counter/search-hits-counter.component.ts
+++ src/app/components/features/search/search-hits-counter/search-hits-counter.component.ts
@@ -1,13 +1,13 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 import { formatNumber } from '../../../../helpers/util.helper';
 import { DetailsService } from '../../../../services/details.service';
 import { SearchService } from '../../../../services/search/search.service';
 
 @Component({
   selector: 'app-search-hits-counter',
-  imports: [NgIf, TranslatePipe],
+  imports: [NgIf],
   templateUrl: './search-hits-counter.component.html',
   styleUrl: './search-hits-counter.component.scss',
 })
