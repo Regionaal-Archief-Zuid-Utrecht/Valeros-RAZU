2c23d534564ab95db45d5fd91a9667c6458a07f8
Simon Dirks <mail@simondirks.com>
2025-07-15 14:57:11 +0200

    Fix issue with details page not loading correctly because of query params

--- src/app/components/features/file-viewers/mirador/mirador.component.ts
+++ src/app/components/features/file-viewers/mirador/mirador.component.ts
@@ -73,7 +73,19 @@ export class MiradorComponent implements OnChanges, OnDestroy, AfterViewInit {
   }
 
   private getPageFromUrl(): number | null {
-    const urlSplitByHashtag = this.router.url.split('%23');
+    const removeQueryParams = (url: string) => {
+      const queryParamIndex = url.indexOf('?');
+      if (queryParamIndex !== -1) {
+        return url.substring(0, queryParamIndex);
+      }
+      return url;
+    };
+
+    let url = removeQueryParams(this.router.url);
+
+    // Double encoded hashtag (%2523): Once by Angular router, once by encodeURIComponent (see Details service)
+    const doubleEncodedHashtag = '%2523';
+    const urlSplitByHashtag = url.split(doubleEncodedHashtag);
     const urlHasHashtag = urlSplitByHashtag.length > 1;
     if (urlHasHashtag) {
       const pageStr: string = urlSplitByHashtag[1];
