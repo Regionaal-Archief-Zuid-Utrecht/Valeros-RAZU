fccdec7a38e58764677afe3b76502f003d5ddfb2
Simon Dirks <mail@simondirks.com>
2025-10-06 11:55:48 +0200

    Show filter options in filter modal

--- src/app/components/features/filters/filter-modal/filter-modal.component.ts
+++ src/app/components/features/filters/filter-modal/filter-modal.component.ts
@@ -1,8 +1,9 @@
 import { Component, type OnInit } from '@angular/core';
+import { FilterOptionsComponent } from "../filter-options/filter-options.component";
 
 @Component({
   selector: 'app-filter-modal',
-  imports: [],
+  imports: [FilterOptionsComponent],
   templateUrl: './filter-modal.component.html',
 })
 export class FilterModalComponent implements OnInit {
