d66c6fed4ee72b66f609894af24cc1feb3f54e22
Simon Dirks <mail@simondirks.com>
2025-10-23 15:34:04 +0200

    Run prettier

--- src/app/components/views/search/search.component.ts
+++ src/app/components/views/search/search.component.ts
@@ -20,6 +20,7 @@ import { SettingsService } from '../../../services/settings.service';
 import { ScrollService } from '../../../services/ui/scroll.service';
 import { UrlService } from '../../../services/url.service';
 import { ViewModeService } from '../../../services/view-mode.service';
+import { SkipLinksComponent } from '../../accessibility/skip-links/skip-links.component';
 import { FilterOptionsComponent } from '../../features/filters/filter-options/filter-options.component';
 import { NodeComponent } from '../../features/node/node.component';
 import { LoadMoreSearchResultsButtonComponent } from '../../features/search/load-more-search-results-button/load-more-search-results-button.component';
@@ -28,7 +29,6 @@ import { SearchHitsCounterComponent } from '../../features/search/search-hits-co
 import { SearchInputComponent } from '../../features/search/search-input/search-input.component';
 import { SortSelectComponent } from '../../features/sort/sort-select/sort-select.component';
 import { ViewModeSelectComponent } from '../../features/view-mode/view-mode-select/view-mode-select.component';
-import { SkipLinksComponent } from '../../accessibility/skip-links/skip-links.component';
 import { DetailsBackButtonComponent } from '../../ui/details-back-button/details-back-button.component';
 import { HeaderComponent } from '../../ui/header/header.component';
 import { LangSwitchComponent } from '../../ui/lang-switch/lang-switch.component';
