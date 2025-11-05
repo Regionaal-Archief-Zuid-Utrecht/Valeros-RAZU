50f79ac9c93a27c7e0c1f2c9e39eff36d00c9715
Simon Dirks <mail@simondirks.com>
2025-11-05 14:47:29 +0100

    Automatically focus on newly added search results when clicking load more

--- src/app/components/features/search/load-more-search-results-button/load-more-search-results-button.component.ts
+++ src/app/components/features/search/load-more-search-results-button/load-more-search-results-button.component.ts
@@ -1,6 +1,8 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { NodeModel } from '../../../../models/node.model';
+import { NodeService } from '../../../../services/node/node.service';
 import { SearchService } from '../../../../services/search/search.service';
 
 @Component({
@@ -10,11 +12,39 @@ import { SearchService } from '../../../../services/search/search.service';
   styleUrl: './load-more-search-results-button.component.scss',
 })
 export class LoadMoreSearchResultsButtonComponent {
-  constructor(public search: SearchService) {}
+  constructor(
+    public search: SearchService,
+    public nodes: NodeService,
+  ) {}
 
-  loadMore() {
+  async loadMore() {
     if (!this.search.isLoading.value) {
-      void this.search.execute(false, false);
+      const oldNumNodes = this.search.results.value.nodes?.length;
+      await this.search.execute(false, false);
+      const newNumNodes = this.search.results.value.nodes?.length;
+
+      const nodesHaveBeenAdded =
+        newNumNodes && oldNumNodes && newNumNodes > oldNumNodes;
+      if (!nodesHaveBeenAdded) {
+        return;
+      }
+
+      const focusOnNewNode = () => {
+        const newNode: NodeModel | undefined =
+          this.search.results.value.nodes?.[oldNumNodes];
+        if (!newNode) {
+          return;
+        }
+        const newNodeId = this.nodes.getId(newNode);
+        const searchResultElem: HTMLElement | null = document.querySelector(
+          `[data-scroll-id="${encodeURIComponent(newNodeId)}"] a.node-result-item`,
+        );
+        searchResultElem?.focus();
+      };
+
+      setTimeout(() => {
+        focusOnNewNode();
+      }, 1);
     }
   }
 }
