404c9ebbdb93edaf26d146b97ae7c6e8c3701fd2
Simon Dirks <mail@simondirks.com>
2025-11-12 13:31:26 +0100

    Add Triply predicate visibility settings

--- src/app/config/triply-settings/triply-settings.ts
+++ src/app/config/triply-settings/triply-settings.ts
@@ -3,6 +3,7 @@ import { defaultSettings } from '../default-settings/default-settings';
 import { triplyEndpointSettings } from './settings/triply-endpoint.settings';
 import { triplyFilteringSettings } from './settings/triply-filtering.settings';
 import { triplyNamespacePrefixes } from './settings/triply-namespace-prefixes.settings';
+import { triplyPredicateVisibilitySettings } from './settings/triply-predicate-visibility.settings';
 import { triplyRenderComponentSettings } from './settings/triply-render-component.settings';
 
 export const triplySettings: SettingsModel = {
@@ -11,4 +12,5 @@ export const triplySettings: SettingsModel = {
   filtering: triplyFilteringSettings,
   namespacePrefixes: triplyNamespacePrefixes,
   renderComponents: triplyRenderComponentSettings,
+  predicateVisibility: triplyPredicateVisibilitySettings,
 };
