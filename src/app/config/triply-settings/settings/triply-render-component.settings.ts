dc5e10522bb93e670b3a682c78d354473adb265a
Simon Dirks <mail@simondirks.com>
2025-11-12 12:22:20 +0100

    Use file renderer component for foaf:depiction and sdo:audio

---
+++ src/app/config/triply-settings/settings/triply-render-component.settings.ts
@@ -0,0 +1,21 @@
+import { FileRendererComponent } from '../../../components/features/node/node-render-components/predicate-render-components/file-renderer/file-renderer.component';
+import {
+  RenderComponentsSettings,
+  RenderMode,
+} from '../../../models/settings/render-component-settings.type';
+import { renderComponentSettings } from '../../default-settings/settings/render-component.settings';
+
+export const triplyRenderComponentSettings: RenderComponentsSettings = {
+  [RenderMode.ByType]: [...renderComponentSettings[RenderMode.ByType]],
+  [RenderMode.ByPredicate]: [
+    ...renderComponentSettings[RenderMode.ByPredicate],
+    {
+      component: FileRendererComponent,
+      predicates: [
+        'http://xmlns.com/foaf/0.1/depiction',
+        'http://schema.org/audio',
+      ],
+      requiresExplicitRendering: true,
+    },
+  ],
+};
--- src/app/config/triply-settings/triply-settings.ts
+++ src/app/config/triply-settings/triply-settings.ts
@@ -3,10 +3,12 @@ import { defaultSettings } from '../default-settings/default-settings';
 import { triplyEndpointSettings } from './settings/triply-endpoint.settings';
 import { triplyFilteringSettings } from './settings/triply-filtering.settings';
 import { triplyNamespacePrefixes } from './settings/triply-namespace-prefixes.settings';
+import { triplyRenderComponentSettings } from './settings/triply-render-component.settings';
 
 export const triplySettings: SettingsModel = {
   ...defaultSettings,
   endpoints: triplyEndpointSettings,
   filtering: triplyFilteringSettings,
   namespacePrefixes: triplyNamespacePrefixes,
+  renderComponents: triplyRenderComponentSettings,
 };
