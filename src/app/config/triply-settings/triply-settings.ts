6f3ccb928e00c994579bfd9743db22d12886d4d0
Simon Dirks <mail@simondirks.com>
2025-11-12 10:58:17 +0100

    Add Triply filter by type

--- src/app/config/triply-settings/triply-settings.ts
+++ src/app/config/triply-settings/triply-settings.ts
@@ -1,8 +1,10 @@
 import { SettingsModel } from '../../models/settings/settings.model';
 import { defaultSettings } from '../default-settings/default-settings';
 import { triplyEndpointSettings } from './settings/triply-endpoint.settings';
+import { triplyFilteringSettings } from './settings/triply-filtering.settings';
 
 export const triplySettings: SettingsModel = {
   ...defaultSettings,
   endpoints: triplyEndpointSettings,
+  filtering: triplyFilteringSettings,
 };
