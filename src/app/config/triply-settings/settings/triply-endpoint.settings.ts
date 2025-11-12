ed9f9e66d7d5da3f527a4b2360f431a91db05d05
Simon Dirks <mail@simondirks.com>
2025-11-12 13:55:37 +0100

    Add Iris flower endpoint

--- src/app/config/triply-settings/settings/triply-endpoint.settings.ts
+++ src/app/config/triply-settings/settings/triply-endpoint.settings.ts
@@ -5,12 +5,22 @@ export const triplyEndpointSettings: EndpointSettings = {
   ...endpointSettings,
   data: {
     pokemon: {
-      label: 'Triply',
+      label: 'Pokemon',
       endpointUrls: [
         {
           elastic:
             'https://triplydb.com/_api/datasets/academy/pokemon/services/search/_search',
           sparql: 'https://api.triplydb.com/datasets/academy/pokemon/sparql',
+        },
+      ],
+    },
+    iris: {
+      label: 'Iris',
+      endpointUrls: [
+        {
+          elastic:
+            'https://triplydb.com/_api/datasets/Triply/iris/services/iris-es/_search',
+          sparql: 'https://api.triplydb.com/datasets/Triply/iris/sparql',
         },
       ],
     },
