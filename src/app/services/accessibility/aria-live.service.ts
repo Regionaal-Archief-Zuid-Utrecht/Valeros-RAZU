1f8ea589158c131e9d5897172aad4f50c879b04b
Simon Dirks <mail@simondirks.com>
2026-06-15 13:52:49 +0200

    fix: use total number of hits for aria-live message
    Instead of number of hits for shown page

--- src/app/services/accessibility/aria-live.service.ts
+++ src/app/services/accessibility/aria-live.service.ts
@@ -14,19 +14,19 @@ export class AriaLiveService {
 
   private initMessageUpdateOnSearchResultsUpdate() {
     this.search.results.subscribe((results) => {
-      const numSearchResults = results.nodes?.length ?? 0;
+      const totalHits = this.search.numberOfHits ?? 0;
       const searchQuery = this.search.queryStr ?? '';
       const searchQueryText = searchQuery ? `voor "${searchQuery}"` : '';
 
-      if (numSearchResults === 0) {
+      if (totalHits === 0) {
         this.message.next(
           `Er zijn geen resultaten gevonden ${searchQueryText}`,
         );
-      } else if (numSearchResults === 1) {
+      } else if (totalHits === 1) {
         this.message.next(`Er is 1 resultaat gevonden ${searchQueryText}`);
       } else {
         this.message.next(
-          `Er zijn ${numSearchResults} resultaten gevonden ${searchQueryText}`,
+          `Er zijn ${totalHits} resultaten gevonden ${searchQueryText}`,
         );
       }
       console.log(this.message.value);
